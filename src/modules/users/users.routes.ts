eb6d8514dc5573735524649beb1624a204c90830
PabloFalc <pablopatricio640@gmail.com>
2025-05-24 14:21:16 -0300

    feat: verificação de email

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,6 +1,7 @@
 import type { FastifyInstance } from "fastify";
 import type { UserCreateInput, UserUpdateInput } from "../../dtos/users.dto";
 import { UsersService } from "./users.service";
+import verifyToken from "../../middlewares/middleware";
 
 export async function UsersRoutes(server: FastifyInstance) {
     const service = new UsersService();
@@ -14,7 +15,7 @@ export async function UsersRoutes(server: FastifyInstance) {
     });
 
     // list & filter
-    server.get("/search", async (_, reply) => {
+    server.get("/search", { preHandler: verifyToken }, async (_, reply) => {
         const result = await service.list();
         return reply
             .code(result.code)
@@ -22,17 +23,22 @@ export async function UsersRoutes(server: FastifyInstance) {
     });
 
     // find by id
-    server.get<{ Params: { id: string } }>("/find/:id", async (req, reply) => {
-        const result = await service.findById(req.params.id);
+    server.get<{ Params: { id: string } }>(
+        "/find/:id",
+        { preHandler: verifyToken },
+        async (req, reply) => {
+            const result = await service.findById(req.params.id);
 
-        return reply
-            .code(result.code)
-            .send({ msg: result.msg, data: result.data });
-    });
+            return reply
+                .code(result.code)
+                .send({ msg: result.msg, data: result.data });
+        }
+    );
 
     // Update
     server.put<{ Params: { id: string }; Body: UserUpdateInput }>(
         "/update",
+        { preHandler: verifyToken },
         async (req, reply) => {
             const result = await service.update(req.params.id, req.body);
 
@@ -43,11 +49,15 @@ export async function UsersRoutes(server: FastifyInstance) {
     );
 
     // Delete
-    server.delete<{ Params: { id: string } }>("/delete", async (req, reply) => {
-        const result = await service.delete(req.params.id);
+    server.delete<{ Params: { id: string } }>(
+        "/delete",
+        { preHandler: verifyToken },
+        async (req, reply) => {
+            const result = await service.delete(req.params.id);
 
-        return reply
-            .code(result.code)
-            .send({ msg: result.msg, data: result.data });
-    });
+            return reply
+                .code(result.code)
+                .send({ msg: result.msg, data: result.data });
+        }
+    );
 }
